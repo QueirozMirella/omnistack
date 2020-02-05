2edc230a52d4a097552015faba647ac54e67e7bd
QueirozMirella <queiroz.av@gmail.com>
2020-02-05 13:40:21 -0300

    hello word

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express'); //agora eu tenho o expressa na variavel express
+
+const app = express(); //crio uma variavel que recebe a função express e agora a aplicação ta criada;
+
+//para eu receber uma resposta, tipo hello word, eu faço o seguinte:
+app.get('/', (request, response) => { //quando eu acessar localhost:3333 eu caio em /users
+    //quando eu acesso alguma rota eu to fazendo uma requisição (request). Todas as informações para criar um registro vão estar dentro ddo request
+    //o response é como a gente vai devolver uma resposta para o nosso cliente
+    return response.send('Hello World'); //o send envia um texto como resposta
+}) 
+
+app.listen(3333); //quando eu acessar localhost:3333 eu acesso o servidor
