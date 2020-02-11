8e188b1ab651b7f87116aad1dd3364d9c88b2eb1
QueirozMirella <queiroz.av@gmail.com>
2020-02-11 10:28:10 -0300

    get, put, post e delete

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,11 @@ const app = express(); //crio uma variavel que recebe a função express e agora
 app.get('/', (request, response) => { //quando eu acessar localhost:3333 eu caio em /users
     //quando eu acesso alguma rota eu to fazendo uma requisição (request). Todas as informações para criar um registro vão estar dentro ddo request
     //o response é como a gente vai devolver uma resposta para o nosso cliente
-    return response.send('Hello World'); //o send envia um texto como resposta
-}) 
+    //return response.send('Hello World'); //o send envia um texto como resposta
+    //nós vamos usar o json, então nao vamos usar o send pra enviar resposta
+    return response.json({
+        message: 'Hello Omnistack'
+    });
+});
 
 app.listen(3333); //quando eu acessar localhost:3333 eu acesso o servidor
