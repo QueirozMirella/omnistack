85b7be29a3929c7c1b1dab2e93d7b3392a8dc96f
QueirozMirella <queiroz.av@gmail.com>
2020-03-05 13:23:49 -0300

    explicando propriedade

--- web/src/App.js
+++ web/src/App.js
@@ -6,7 +6,11 @@ import Header from './Header';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header title="title 1" />
+      <Header title="title 2" />
+      <Header title="title 3" />
+    </>
   );
 }
 
@@ -14,4 +18,4 @@ export default App;
 
 //componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação.
 //estado:
-//propriedade:
+//propriedade: Informações que um componente pai passa para o componente filho
